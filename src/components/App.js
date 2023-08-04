1da8dde5a6e5d507fabeb04c7e56c4ac09f22471
shrutijain2296 <shrutijain2296@gmail.com>
2023-08-04 21:21:13 +0530

    submitting code-Fri Aug 04 2023 21:21:12 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
-import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <TodoList />
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+
+
+const TodoList = () =>{
+let [listItem, setListItem] = useState([]);
+let [newItem, setNewItem] = useState("");
+
+function addItems(event){
+    event.preventDefault();
+
+    if(newItem.trim !== ""){
+        setListItem([...listItem, newItem]);
+        setNewItem("");
+    }    
+}
+
+function removeItem(index){
+    let updatedList = ([...listItem]);
+    updatedList.splice(index, 1);
+    setListItem(updatedList);
+}
+
+    return(
+        <div>
+            <h1>To-Do-List</h1>
+            <input type = "text" value = {newItem} onChange = {(event) => setNewItem(event.target.value)} />
+            <button onClick = {addItems}>Add Todo</button>
+            {
+                listItem.map((item, index) =>(
+                    <div key = {index}>
+                        <p>{item}</p>
+                        <button onClick={() => removeItem(index)}>Delete</button>
+                    </div>
+                
+                )
+
+                )
+            }
+        </div>
+    )
+}
+export default TodoList;
